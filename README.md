1b82d3ee08f77bb77adbdb7e756475ba0d1bb520
Olivier Refalo <orefalo@snapcore.com>
2024-11-19 23:25:34 +0400

    add anthropic Claude

--- README.md
+++ README.md
@@ -1,16 +1,43 @@
-# ChatGPT_Superset.popclipext
+# LLM_Superset.popclipext
+
+<center>
+  <img src="chatgpt-icon.svg" width="80px"/>
+  <img src="claude-ai-icon.svg" width="80px"/>
+</center>
 
 ![screenshot1](screenshot1.png)
 
 ![screenshot2](screenshot2.png)
 
-This is my custom CharGPT extension for PopClip. Easy to modify; simply edit the code and JavaScript file.
+This is my custom LLM extension for PopClip supporting some of the best LLM out there. 
+
+* Supports OpenAI ChatGPT and Anthropic Claude
+* Tone settings: professional, concise, and friendly
+* Text improvements, corrections, enhancements, and summarization
+* Translation into multiple languages
+* Holding **SHIFT** copies the response to clipboard
+* Easy to modify by editing the code and JavaScript file
 
 ## Install
 
-1. Clone the repo  ** git clone <repo url> **
-2. Double click on the toplevel folder with the **.popclipext**
+### Option 1
+
+1. Download LLM_Superset.popclipextz
+2. Double click the file
 3. Follow PopClip instructions
+
+### Option 2
+
+1. Clone the repo  **git clone <this repo url>**
+2. Double click on the toplevel folder, LLM_Superset.popclipext
+3. Follow PopClip instructions
+
+### Configuration
+
+You need to create API keys with the services to enable the integration. Use the links below to activate them.
+
+* https://platform.openai.com/account/api-keys
+* https://console.anthropic.com/settings/keys
 
 ## Debug
 
--- llm-superset.js
+++ llm-superset.js
@@ -1,29 +1,74 @@
-const openai = require("axios").create({
-  baseURL: "https://api.openai.com/v1/",
-});
+
+const axios = require("axios");
 
 function prepareResponse(data) {
-  const response = data.choices[0].message.content.trim();
   // if holding shift, copy just the response. else, paste the last input and response.
   if (popclip.modifiers.shift) {
-    popclip.copyText(response);
+    popclip.copyText(data);
   } else {
-    popclip.pasteText(response);
+    popclip.pasteText(data);
   }
 }
 
+async function callLLMapi(prompt, options) {
+  if (options.model.startsWith("gpt")) {
+    return await callOpenAPI(prompt, options);
+  } else if (options.model.startsWith("claude")) {
+    return await callClaudeAPI(prompt, options);
+  } else throw new Error("Unknown model selection:" + options.model);
+}
+
+// --- CLAUDE
+async function callClaudeAPI(prompt, options) {
+  const key = options.claudeapikey.trim();
+  if (!key || key.length === 0)
+    throw new Error("Settings error: missing ClaudeAPI key");
+
+  const requestConfig = {
+    headers: {
+      "x-api-key": key,
+      "anthropic-version": "2023-06-01",
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  };
+
+  const messages = [{ role: "user", content: prompt }];
+
+  const { data } = await axios.post(
+    "https://api.anthropic.com/v1/messages",
+    {
+      model: options.model,
+      max_tokens: 2048,
+      messages,
+    },
+    requestConfig
+  );
+  return data.content[0].text.trim();
+}
+
+// --- OPENAI
 async function callOpenAPI(prompt, options) {
   const key = options.apikey.trim();
   if (!key || key.length === 0)
     throw new Error("Settings error: missing OpenAPI key");
 
-  openai.defaults.headers.common.Authorization = `Bearer ${key}`;
+  const requestConfig = {
+    headers: {
+      Authorization: `Bearer ${key}`,
+    },
+  };
+
   const messages = [{ role: "user", content: prompt }];
-  const { data } = await openai.post("chat/completions", {
-    model: options.model,
-    messages,
-  });
-  return data;
+  const { data } = await axios.post(
+    "https://api.openai.com/v1/chat/completions",
+    {
+      model: options.model,
+      messages,
+    },
+    requestConfig
+  );
+  return data.choices[0].message.content.trim();
 }
 
 async function improveWritting(input, options) {
@@ -33,7 +78,7 @@ async function improveWritting(input, options) {
     " tone, avoid complex words and verbs. I want you to only reply the correction, the improvements and nothing else, do not write explanations. Correct and improve the following sentence: \n\n" +
     input.text.trim();
 
-  const data = await callOpenAPI(prompt, options);
+  const data = await callLLMapi(prompt, options);
   prepareResponse(data);
 }
 
@@ -44,7 +89,7 @@ async function spellingAndGrammar(input, options) {
     " tone: \n\n" +
     input.text.trim();
 
-  const data = await callOpenAPI(prompt, options);
+  const data = await callLLMapi(prompt, options);
   prepareResponse(data);
 }
 
@@ -53,8 +98,8 @@ async function summarize(input, options) {
     "You are a highly skilled AI trained in language comprehension and summarization. I would like you to read the following text and summarize it into a concise abstract paragraph. Aim to retain the most important points, providing a coherent and readable summary that could help a person understand the main points of the discussion without needing to read the entire text. avoid unnecessary details or tangential points: \n\n" +
     input.text.trim();
 
-  const data = await callOpenAPI(prompt, options);
-  return data.choices[0].message.content.trim();
+  const data = await callLLMapi(prompt, options);
+  prepareResponse(data);
 }
 
 async function makeLonger(input, options) {
@@ -62,7 +107,7 @@ async function makeLonger(input, options) {
     "I'll give you text. You'll rewrite it and output it longer Keep the meaning the same as well as the language. Only give me the output and nothing else. Now, using the concepts above, re-write the following text. Respond in the same language variety or dialect of the following text: \n\n" +
     input.text.trim();
 
-  const data = await callOpenAPI(prompt, options);
+  const data = await callLLMapi(prompt, options);
   prepareResponse(data);
 }
 
@@ -71,7 +116,7 @@ async function makeShorter(input, options) {
     "I'll give you text. You'll rewrite it and output it shorter to be no more than half the number of characters of the original text.Keep the meaning the same. Only give me the output and nothing else.Now, using the concepts above, re-write the following text. Respond in the same language variety or dialect of the following text: \n\n" +
     input.text.trim();
 
-  const data = await callOpenAPI(prompt, options);
+  const data = await callLLMapi(prompt, options);
   prepareResponse(data);
 }
 
@@ -82,7 +127,7 @@ async function translate(input, options) {
     " language. Keep the meaning the same. Do not alter the original structure and formatting outlined in any way. Only give me the output and nothing else: \n\n" +
     input.text.trim();
 
-  const data = await callOpenAPI(prompt, options);
+  const data = await callLLMapi(prompt, options);
   prepareResponse(data);
 }
 
